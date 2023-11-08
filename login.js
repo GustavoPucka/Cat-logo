9c9a93a016daef9037d46f296817999b68e3f4a3
GustavoPucka <gustavopucka16112007@gmail.com>
2023-11-08 17:34:29 -0300

    catálogo da audi

---
+++ login.js
@@ -0,0 +1,8 @@
+// Função para exibir a mensagem de login com sucesso
+function showLoginSuccessMessage() {
+    const loginSuccessMessage = document.getElementById("login-success-message");
+    loginSuccessMessage.style.display = "block";
+  }
+
+  // Chamando a função para exibir a mensagem (você pode chamar isso após o login ser bem-sucedido)
+  showLoginSuccessMessage();
